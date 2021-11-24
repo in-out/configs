d618dd2f7a2e1d38de8bc2780bf25c4d6390dbe9
in-out <oneofsuns@gmail.com>
2021-11-24 23:59:50 +0800

    Update sfkeys.js

--- sfkeys.js
+++ sfkeys.js
@@ -261,7 +261,7 @@ mapkey("-e", "google translate", () => {
     mapkey('gc', '#12Open Chrome Configure', function() {
         tabOpenLink("chrome://settings/");
     });
-settings.blacklistPattern = /.*mail.google.com.*|.*inbox.google.com.*|trello.com|inoreader.com|route.asus.com|localhost/i;
+settings.blacklistPattern = /.*mail.google.com.*|.*inbox.google.com.*|trello.com|feishu.cn|inoreader.com|route.asus.com|localhost/i;
 settings.nextLinkRegex = /((>>|下一页|next|older posts|older entries|forward|>|>>|❯|›|»|more|下一张|下页|(阅读)?下一章|次へ)+)/i;
 settings.prevLinkRegex = /((<<|上一页|prev(ious)?|newer posts|newer entries|back|<|❮|<<«|less|‹|上一张|(阅读)?上一章|前へ)+)/i;
 
