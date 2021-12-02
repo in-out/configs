1e71dbf3bc42ba7560b28469eb08884abf42fe0f
in-out <oneofsuns@gmail.com>
2021-12-02 18:46:54 +0800

    Update sfkeys.js

--- sfkeys.js
+++ sfkeys.js
@@ -101,7 +101,7 @@ addSearchAliasX('js', 'jianshu', 'https://www.jianshu.com/search?q=', 's');
 addSearchAliasX('gh', 'github', 'https://github.com/search?q=', 's');
 addSearchAliasX('rr', 'rarbg', 'https://rarbg.to/torrents.php?search=', 's');
 //addSearchAliasX('ms', 'mengso', 'https://mengso.com/search?q=', 's');
-addSearchAliasX('gg', 'google', 'https://www.google.com/search?q=', 's');
+addSearchAliasX('gg', 'google', 'https://www.google.com/search?pws=0&gl=us&gws_rd=cr&q=', 's');
 
 //mapkey('osx', 'call searx', function() {
 //	Front.openOmnibar({
