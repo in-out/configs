cd26b6d977131cda410dabbb161877c9e51e3196
in-out <oneofsuns@gmail.com>
2022-04-10 22:21:15 +0800

    Update surfingkeys-1.x.js

--- surfingkeys-1.x.js
+++ surfingkeys-1.x.js
@@ -263,66 +263,66 @@ mapkey("-e", "google translate", () => {
   }
 });
 
-//Front.registerInlineQuery({
-//    url: function(q) {
-//        return `http://dict.youdao.com/w/eng/${q}/#keyfrom=dict2.index`;
-//    },
-//    parseResult: function(res) {
-//        var parser = new DOMParser();
-//        var doc = parser.parseFromString(res.text, "text/html");
-//        var collinsResult = doc.querySelector("#collinsResult");
-//        var authTransToggle = doc.querySelector("#authTransToggle");
-//        var examplesToggle = doc.querySelector("#examplesToggle");
-//        if (collinsResult) {
-//            collinsResult.querySelectorAll("div>span.collinsOrder").forEach(function(span) {
-//                span.nextElementSibling.prepend(span);
-//            });
-//            collinsResult.querySelectorAll("div.examples").forEach(function(div) {
-//                div.innerHTML = div.innerHTML.replace(/<p/gi, "<span").replace(/<\/p>/gi, "</span>");
-//            });
-//            var exp = collinsResult.innerHTML;
-//            return exp;
-//        } else if (authTransToggle) {
-//            authTransToggle.querySelector("div.via.ar").remove();
-//            return authTransToggle.innerHTML;
-//        } else if (examplesToggle) {
-//            return examplesToggle.innerHTML;
-//        }
-//    }
-//});
-
 Front.registerInlineQuery({
     url: function(q) {
-        return `https://jisho.org/search/${q}`;
+        return `http://dict.youdao.com/w/eng/${q}/#keyfrom=dict2.index`;
     },
     parseResult: function(res) {
         var parser = new DOMParser();
         var doc = parser.parseFromString(res.text, "text/html");
-        var result = doc.querySelector("#primary>div.exact_block");
-        if (result) {
-            result.querySelectorAll('div>span.furigana').forEach(function(e){
-                br = document.createElement("br");
-                e.appendChild(br);
+        var collinsResult = doc.querySelector("#collinsResult");
+        var authTransToggle = doc.querySelector("#authTransToggle");
+        var examplesToggle = doc.querySelector("#examplesToggle");
+        if (collinsResult) {
+            collinsResult.querySelectorAll("div>span.collinsOrder").forEach(function(span) {
+                span.nextElementSibling.prepend(span);
             });
-            result.querySelectorAll('h4').forEach(function(e){
-                e.remove();
+            collinsResult.querySelectorAll("div.examples").forEach(function(div) {
+                div.innerHTML = div.innerHTML.replace(/<p/gi, "<span").replace(/<\/p>/gi, "</span>");
             });
-            result.querySelectorAll('div>div.concept_light-status').forEach(function(e){
-                e.remove();
-            });
-            result.querySelectorAll('div>a.light-details_link').forEach(function(e){
-                e.remove();
-            });
-            result.querySelectorAll('div>span.meaning-abstract').forEach(function(e){
-                e.remove();
-            });
-            result.querySelectorAll('div>span.supplemental_info').forEach(function(e){
-                e.outerHTML = "&nbsp;" + e.outerHTML;
-            });
-            var exp = result.innerHTML;
+            var exp = collinsResult.innerHTML;
             return exp;
-        }    }
+        } else if (authTransToggle) {
+            authTransToggle.querySelector("div.via.ar").remove();
+            return authTransToggle.innerHTML;
+        } else if (examplesToggle) {
+            return examplesToggle.innerHTML;
+        }
+    }
 });
+
+//Front.registerInlineQuery({
+//    url: function(q) {
+//        return `https://jisho.org/search/${q}`;
+//    },
+//    parseResult: function(res) {
+//        var parser = new DOMParser();
+//        var doc = parser.parseFromString(res.text, "text/html");
+//        var result = doc.querySelector("#primary>div.exact_block");
+//        if (result) {
+//            result.querySelectorAll('div>span.furigana').forEach(function(e){
+//                br = document.createElement("br");
+//                e.appendChild(br);
+//            });
+//            result.querySelectorAll('h4').forEach(function(e){
+//                e.remove();
+//            });
+//            result.querySelectorAll('div>div.concept_light-status').forEach(function(e){
+//                e.remove();
+//            });
+//            result.querySelectorAll('div>a.light-details_link').forEach(function(e){
+//                e.remove();
+//            });
+//            result.querySelectorAll('div>span.meaning-abstract').forEach(function(e){
+//                e.remove();
+//            });
+//            result.querySelectorAll('div>span.supplemental_info').forEach(function(e){
+//                e.outerHTML = "&nbsp;" + e.outerHTML;
+//            });
+//            var exp = result.innerHTML;
+//            return exp;
+//        }    }
+//});
 // an example to replace `u` with `?`, click `Default mappings` to see how `u` works.
 //map('?', 'u');
 //map('J','E');
