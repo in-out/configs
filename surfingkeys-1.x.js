a6f532aa67bd85249bd244b3e44b69f7a407336b
in-out <oneofsuns@gmail.com>
2022-04-10 22:15:17 +0800

    Update surfingkeys-1.x.js

--- surfingkeys-1.x.js
+++ surfingkeys-1.x.js
@@ -263,32 +263,65 @@ mapkey("-e", "google translate", () => {
   }
 });
 
+//Front.registerInlineQuery({
+//    url: function(q) {
+//        return `http://dict.youdao.com/w/eng/${q}/#keyfrom=dict2.index`;
+//    },
+//    parseResult: function(res) {
+//        var parser = new DOMParser();
+//        var doc = parser.parseFromString(res.text, "text/html");
+//        var collinsResult = doc.querySelector("#collinsResult");
+//        var authTransToggle = doc.querySelector("#authTransToggle");
+//        var examplesToggle = doc.querySelector("#examplesToggle");
+//        if (collinsResult) {
+//            collinsResult.querySelectorAll("div>span.collinsOrder").forEach(function(span) {
+//                span.nextElementSibling.prepend(span);
+//            });
+//            collinsResult.querySelectorAll("div.examples").forEach(function(div) {
+//                div.innerHTML = div.innerHTML.replace(/<p/gi, "<span").replace(/<\/p>/gi, "</span>");
+//            });
+//            var exp = collinsResult.innerHTML;
+//            return exp;
+//        } else if (authTransToggle) {
+//            authTransToggle.querySelector("div.via.ar").remove();
+//            return authTransToggle.innerHTML;
+//        } else if (examplesToggle) {
+//            return examplesToggle.innerHTML;
+//        }
+//    }
+//});
+
 Front.registerInlineQuery({
     url: function(q) {
-        return `http://dict.youdao.com/w/eng/${q}/#keyfrom=dict2.index`;
+        return `https://jisho.org/search/${q}`;
     },
     parseResult: function(res) {
         var parser = new DOMParser();
         var doc = parser.parseFromString(res.text, "text/html");
-        var collinsResult = doc.querySelector("#collinsResult");
-        var authTransToggle = doc.querySelector("#authTransToggle");
-        var examplesToggle = doc.querySelector("#examplesToggle");
-        if (collinsResult) {
-            collinsResult.querySelectorAll("div>span.collinsOrder").forEach(function(span) {
-                span.nextElementSibling.prepend(span);
+        var result = doc.querySelector("#primary>div.exact_block");
+        if (result) {
+            result.querySelectorAll('div>span.furigana').forEach(function(e){
+                br = document.createElement("br");
+                e.appendChild(br);
             });
-            collinsResult.querySelectorAll("div.examples").forEach(function(div) {
-                div.innerHTML = div.innerHTML.replace(/<p/gi, "<span").replace(/<\/p>/gi, "</span>");
+            result.querySelectorAll('h4').forEach(function(e){
+                e.remove();
             });
-            var exp = collinsResult.innerHTML;
+            result.querySelectorAll('div>div.concept_light-status').forEach(function(e){
+                e.remove();
+            });
+            result.querySelectorAll('div>a.light-details_link').forEach(function(e){
+                e.remove();
+            });
+            result.querySelectorAll('div>span.meaning-abstract').forEach(function(e){
+                e.remove();
+            });
+            result.querySelectorAll('div>span.supplemental_info').forEach(function(e){
+                e.outerHTML = "&nbsp;" + e.outerHTML;
+            });
+            var exp = result.innerHTML;
             return exp;
-        } else if (authTransToggle) {
-            authTransToggle.querySelector("div.via.ar").remove();
-            return authTransToggle.innerHTML;
-        } else if (examplesToggle) {
-            return examplesToggle.innerHTML;
-        }
-    }
+        }    }
 });
 // an example to replace `u` with `?`, click `Default mappings` to see how `u` works.
 //map('?', 'u');
@@ -313,7 +346,6 @@ settings.prevLinkRegex = /((<<|上一页|prev(ious)?|newer posts|newer entries|�
 // Prevent automatic next/previous page loads
 settings.smartPageBoundary = false;
 settings.stealFocusOnLoad = false;
-
 // Tomorrow-Night
 Hints.style('border: solid 2px #373B41; color:#52C196; background: initial; background-color: #1D1F21;');
 Hints.style("border: solid 2px #373B41 !important; padding: 1px !important; color: #C5C8C6 !important; background: #1D1F21 !important;", "text");
@@ -348,16 +380,15 @@ settings.theme = `
 /* Edit these variables for easy theme making */
 :root {
   /* Font */
-  --font: 'Microsoft YaHei', 'FontAwesome',  'Meiryo', 'Source Code Pro', Ubuntu, sans;
-  --font-size: 18;
+  --font: 'Source Code Pro', Ubuntu, sans;
+  --font-size: 12;
   --font-weight: bold;
-    /* -------------- */
+  /* -------------- */
   /* --- THEMES --- */
   /* -------------- */
   /* -------------------- */
   /* -- Tomorrow Night -- */
   /* -------------------- */
-  /* -- DELETE LINE TO ENABLE THEME
   --fg: #C5C8C6;
   --bg: #282A2E;
   --bg-dark: #1D1F21;
@@ -366,7 +397,6 @@ settings.theme = `
   --accent-fg: #52C196;
   --info-fg: #AC7BBA;
   --select: #585858;
-  -- DELETE LINE TO ENABLE THEME */
   /* Unused Alternate Colors */
   /* --cyan: #4CB3BC; */
   /* --orange: #DE935F; */
@@ -375,6 +405,7 @@ settings.theme = `
   /* -------------------- */
   /* --      NORD      -- */
   /* -------------------- */
+  /* -- DELETE LINE TO ENABLE THEME
   --fg: #E5E9F0;
   --bg: #3B4252;
   --bg-dark: #2E3440;
@@ -383,6 +414,7 @@ settings.theme = `
   --accent-fg: #A3BE8C;
   --info-fg: #5E81AC;
   --select: #4C566A;
+  -- DELETE LINE TO ENABLE THEME */
   /* Unused Alternate Colors */
   /* --orange: #D08770; */
   /* --red: #BF616A; */
@@ -425,5 +457,182 @@ settings.theme = `
   /* --blue: #268BD2; */
   /* --violet: #9C91E4; */
   /* --cyan: #66D9EF; */
+}
+/* ---------- Generic ---------- */
+.sk_theme {
+background: var(--bg);
+color: var(--fg);
+  background-color: var(--bg);
+  border-color: var(--border);
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+input {
+  font-family: var(--font);
+  font-weight: var(--font-weight);
+}
+.sk_theme tbody {
+  color: var(--fg);
+}
+.sk_theme input {
+  color: var(--fg);
+}
+/* Hints */
+#sk_hints .begin {
+  color: var(--accent-fg) !important;
+}
+#sk_tabs .sk_tab {
+  background: var(--bg-dark);
+  border: 1px solid var(--border);
+}
+#sk_tabs .sk_tab_title {
+  color: var(--fg);
+}
+#sk_tabs .sk_tab_url {
+  color: var(--main-fg);
+}
+#sk_tabs .sk_tab_hint {
+  background: var(--bg);
+  border: 1px solid var(--border);
+  color: var(--accent-fg);
+}
+.sk_theme #sk_frame {
+  background: var(--bg);
+  opacity: 0.2;
+  color: var(--accent-fg);
+}
+/* ---------- Omnibar ---------- */
+/* Uncomment this and use settings.omnibarPosition = 'bottom' for Pentadactyl/Tridactyl style bottom bar */
+/* .sk_theme#sk_omnibar {
+  width: 100%;
+  left: 0;
+} */
+.sk_theme .title {
+  color: var(--accent-fg);
+}
+.sk_theme .url {
+  color: var(--main-fg);
+}
+.sk_theme .annotation {
+  color: var(--accent-fg);
+}
+.sk_theme .omnibar_highlight {
+  color: var(--accent-fg);
+}
+.sk_theme .omnibar_timestamp {
+  color: var(--info-fg);
+}
+.sk_theme .omnibar_visitcount {
+  color: var(--accent-fg);
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+  background: var(--bg-dark);
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+  background: var(--border);
+}
+.sk_theme #sk_omnibarSearchArea {
+  border-top-color: var(--border);
+  border-bottom-color: var(--border);
+}
+.sk_theme #sk_omnibarSearchArea input,
+.sk_theme #sk_omnibarSearchArea span {
+  font-size: var(--font-size);
+}
+.sk_theme .separator {
+  color: var(--accent-fg);
+}
+/* ---------- Popup Notification Banner ---------- */
+#sk_banner {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+  background: var(--bg);
+  border-color: var(--border);
+  color: var(--fg);
+  opacity: 0.9;
+}
+/* ---------- Popup Keys ---------- */
+#sk_keystroke {
+  background-color: var(--bg);
+}
+.sk_theme kbd .candidates {
+  color: var(--info-fg);
+}
+.sk_theme span.annotation {
+  color: var(--accent-fg);
+}
+/* ---------- Popup Translation Bubble ---------- */
+#sk_bubble {
+  background-color: var(--bg) !important;
+  color: var(--fg) !important;
+  border-color: var(--border) !important;
+}
+#sk_bubble * {
+  color: var(--fg) !important;
+}
+#sk_bubble div.sk_arrow div:nth-of-type(1) {
+  border-top-color: var(--border) !important;
+  border-bottom-color: var(--border) !important;
+}
+#sk_bubble div.sk_arrow div:nth-of-type(2) {
+  border-top-color: var(--bg) !important;
+  border-bottom-color: var(--bg) !important;
+}
+/* ---------- Search ---------- */
+#sk_status,
+#sk_find {
+  font-size: var(--font-size);
+  border-color: var(--border);
+}
+.sk_theme kbd {
+  background: var(--bg-dark);
+  border-color: var(--border);
+  box-shadow: none;
+  color: var(--fg);
+}
+.sk_theme .feature_name span {
+  color: var(--main-fg);
+}
+/* ---------- ACE Editor ---------- */
+#sk_editor {
+  background: var(--bg-dark) !important;
+  height: 50% !important;
+  /* Remove this to restore the default editor size */
+}
+.ace_dialog-bottom {
+  border-top: 1px solid var(--bg) !important;
+}
+.ace-chrome .ace_print-margin,
+.ace_gutter,
+.ace_gutter-cell,
+.ace_dialog {
+  background: var(--bg) !important;
+}
+.ace-chrome {
+  color: var(--fg) !important;
+}
+.ace_gutter,
+.ace_dialog {
+  color: var(--fg) !important;
+}
+.ace_cursor {
+  color: var(--fg) !important;
+}
+.normal-mode .ace_cursor {
+  background-color: var(--fg) !important;
+  border: var(--fg) !important;
+  opacity: 0.7 !important;
+}
+.ace_marker-layer .ace_selection {
+  background: var(--select) !important;
+}
+.ace_editor,
+.ace_dialog span,
+.ace_dialog input {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
 }
 `;
