d969d34e8a57b76147280828308b3505eb1f5996
in-out <oneofsuns@gmail.com>
2022-04-20 17:51:38 +0800

    Create font.js

---
+++ font.js
@@ -0,0 +1,105 @@
+#attribution.attribution {
+    display: none;
+}
+#go_top {
+    font-family: "w3cplus";
+}
+* {
+    -webkit-text-stroke-width: 0.5px;
+}
+*:not(i),
+.MBeuO,
+.gsfi,
+.lst,
+.xst,
+.form-control {
+    font-family: "HarmonyOS Sans", "Microsoft YaHei", "Inconsolata", "Tahoma", "SimSun", "FontAwesome", "Segoe UI", "Segoe UI Symbol", "Symbola", "Meiryo", "Malgun Gothic", "NSimSun", "FZSongS", "方正宋体S-超大字符集", "Arial Unicode MS";
+}
+.iconfont > em {
+    font-family: "shop-iconfont", "global-iconfont";
+}
+.pointsPanel h2 > span,
+.pointsPanel h2 > strong,
+#indexbutton,
+#loading > div {
+    font-family: "League Gothic", "html5test";
+}
+.s-btn-container > input {
+    font-family: "globalheader";
+}
+.social li a {
+    font-family: "JustVectorRegular";
+}
+.toolbox ins a,
+.tool_ul li a ins,
+.panel ins a,
+.kz_tit > a > ins {
+    font-family: "2345";
+}
+a.data-send,
+i.current,
+.item-second > i,
+span.dialog-ext-close-x,
+#mxevt-0 > i,
+#mxevt-1 > i,
+.item-first > i {
+    font-family: "uxiconfont";
+}
+/*
+::-webkit-scrollbar {
+    height: 3px;
+    width: 3px;
+}
+
+::-webkit-scrollbar-thumb:horizontal {
+    -webkit-border-radius: 3px;
+    background-color: #666;
+    width: 5px;
+}
+
+::-webkit-scrollbar-thumb:vertical {
+    -webkit-border-radius: 3px;
+    background-color: #666;
+    height: 5px;
+}
+
+::-webkit-scrollbar-track-piece {
+    -webkit-border-radius: 3px;
+    background-color: #ffffff;
+}*/
+/** {
+    font-family: "Microsoft YaHei" ;
+}*/
+/*a {
+text-decoration: none !important;
+-webkit-transition: all 200ms;
+}
+a:hover {
+color: white !important;
+background-color: #9F9F9F !important;
+-webkit-border-radius: 2px;
+-webkit-box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.33);
+-webkit-transition: all 100ms;
+}*/
+input[type="checkbox"]:focus,
+input[type="submit"]:focus,
+input[type="reset"]:focus,
+input[type="radio"]:focus {
+    outline: 1px solid #4C7CD0;
+}
+input[type="text"]:focus,
+input[type="password"]:focus,
+textarea:focus {
+    -webkit-box-shadow: 0px 0px 0px #4C7CD0;
+    outline: 2px solid #4C7CD0;
+}
+span.diffstat-bar,
+span.diff-added {
+    font-family: "octicons";
+}
+span.fui-logo-text {
+    font-family: "IterCast-Icon";
+}
+span.nba_matchnav_score {
+    font-family: "System", "SimSun";
+}
