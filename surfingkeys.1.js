ea791aef3e7192ad4f9b31a04a10b052d211a464
in-out <oneofsuns@gmail.com>
2022-11-13 20:18:25 +0800

    Update surfingkeys.1.js

--- surfingkeys.1.js
+++ surfingkeys.1.js
@@ -160,14 +160,14 @@ mapkey("wg", "add to girls", function () {
 );
 
 mapkey("a1", "rotate 90 degrees", function () {
-  var artplayer = document.getElementsByClassName('art-video');
-  if (artplayer[0]) artplayer[0].style.transform = 'rotate(90deg)';
+  var artplayer = document.getElementsByClassName('art-video')[0];
+  if (artplayer) artplayer.style.transform = 'rotate(90deg)';
 }
 );
 
 mapkey("a2", "rotate 0 degrees", function () {
-  var artplayer = document.getElementsByClassName('art-video');
-  if (artplayer[0]) artplayer[0].style.transform = 'rotate(0deg)';
+  var artplayer = document.getElementsByClassName('art-video')[0];
+  if (artplayer) artplayer.style.transform = 'rotate(0deg)';
 }
 );
 
