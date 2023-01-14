61a5e14f6fb861974d3928c9712144f2d5b5f270
in-out <oneofsuns@gmail.com>
2023-01-14 14:45:47 +0800

    Update surfingkeys.1.js

--- surfingkeys.1.js
+++ surfingkeys.1.js
@@ -102,7 +102,7 @@ addSearchAlias('gg', 'google', 'https://www.google.com/search?pws=0&gl=us&gws_rd
 addSearchAlias('gy', 'youtube', 'https://www.youtube.com/results?search_query=', 's');
 addSearchAlias('dk', 'docker', 'https://hub.docker.com/search?q=', 's');
 addSearchAlias('jb', 'jable', 'https://jable.tv/search/{0}/', 's');
-addSearchAlias('zz', 'zhaoziyuan', 'https://zhaoziyuan.me/so?filename=', 's');
+addSearchAlias('zz', 'zhaoziyuan', 'https://zhaoziyuan.la/so?filename=', 's');
 addSearchAlias('zs', 'assrt', 'https://assrt.net/sub/?searchword=', 's');
 addSearchAlias('zd', 'subhd', 'https://subhd.tv/search/', 's');
 addSearchAlias('za', 'a4k', 'https://www.a4k.net/search?term=', 's');
