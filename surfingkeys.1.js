4e326ead3807d80cf3ce11d9ecacf02b61b1d1af
in-out <oneofsuns@gmail.com>
2024-06-25 19:29:30 +0800

    Update surfingkeys.1.js

--- surfingkeys.1.js
+++ surfingkeys.1.js
@@ -488,8 +488,7 @@ mapkey("oav", "Open xxx", () => {
     tabOpenLink(
       'https://www.seejav.shop/forum/\nhttps://t66y.com/thread0806.php?fid=16\nhttps://t0713.wonderfulday27.live/forumdisplay.php?fid=19&filter=digest\nhttps://t0713.wonderfulday27.live/forumdisplay.php?fid=21&filter=digest\nhttps://t0713.wonderfulday27.live/forumdisplay.php?fid=4&filter=digest\nhttps://jable.tv/latest-updates/'
     );
-  }
-});
+  });
 
 Front.registerInlineQuery({
   url: function (q) {
