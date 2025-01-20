e4f4e2a5e2b4eee99b7e94c4aaa63e3ca825c2fd
in-out <oneofsuns@gmail.com>
2025-01-20 23:38:35 +0800

    Update surfingkeys.1.js

--- surfingkeys.1.js
+++ surfingkeys.1.js
@@ -660,7 +660,7 @@ mapkey('gc', '#12Open Chrome Configure', function () {
   tabOpenLink("chrome://settings/");
 });
 settings.lurkingPattern = /.*jable.*|.*fs1\.app|.*missav.*|.*youtube.*|.*playav.*|.*taiav.*|yalist\.unionall\.fun|.*hsex\.men.*|.*rou\.video.*|.*pan1\.me.*/i;
-settings.blocklistPattern = /.*mail.google.com.*|.*inbox.google.com.*|trello.com|feishu.cn|inoreader.com|route.asus.com|rsshub.unionall.fun|yarr.unionall.fun|.*bilibili|.*acfun.*|localhost|114\.96/i;
+settings.blocklistPattern = /.*mail.google.com.*|.*inbox.google.com.*|trello.com|feishu.cn|inoreader.com|route.asus.com|rsshub.unionall.fun|yarr.unionall.fun|yarrr.unionall.fun|.*bilibili|.*acfun.*|localhost|114\.96/i;
 settings.nextLinkRegex = /((>>|下一页|next|older posts|older entries|forward|Next »|>|>>|❯|›|»|more|下一张|下页|(阅读)?下一章|下一章|下一頁|次へ)+)/i;
 settings.prevLinkRegex = /((<<|上一页|prev(ious)?|newer posts|newer entries|« Previous|back|<|❮|<<«|less|‹|上一张|(阅读)?上一章|上一章|前へ)+)/i;
 
